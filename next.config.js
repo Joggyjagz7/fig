3b0feb782f2e336e379d4c082d6f68ebe0c29e44
shunGoes <shunnode@gmail.com>
2025-02-10 15:42:51 +0100

    pushing latest changes

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true, // Ensures proper routing
+  };
+  
+  module.exports = nextConfig;
+  
--- src/app/fonts.js
+++ src/app/fonts.js
@@ -1,10 +1,4 @@
-import { DM_Sans,  Open_Sans, Nunito_Sans} from "next/font/google"
-
-export const OpenSans = Open_Sans({
-  subsets: ['latin'],
-  weight: ['700'], 
-  style: ['normal', 'italic'], 
-});
+import { DM_Sans, Nunito_Sans} from "next/font/google"
 
 export const dm_sans = DM_Sans({
     subsets: ['latin'],
