b0ca14030869c0cebae76969642b3e4e40a39fe4
shunGoes <shunnode@gmail.com>
2025-02-11 10:04:46 +0100

    added favicon

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    images: {
+        unoptimized: true, // Disables next/image optimization
+      },
     trailingSlash: true, // Ensures proper routing
   };
   
