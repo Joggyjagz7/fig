f9dd1cd7f20526ea1cce7f9d79c6ddee298f32fb
Ejiro Onose <ejiro10000@gmail.com>
2025-02-13 16:39:54 +0100

    Update fonts.js
    added two new fonts

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -1,12 +1,27 @@
-import { DM_Sans, Nunito_Sans} from "next/font/google"
+import { DM_Sans, Nunito_Sans, Open_Sans, Bricolage_Grotesque} from "next/font/google"
 
 export const dm_sans = DM_Sans({
     subsets: ['latin'],
     weight: ['400', '700', '800', "900"], 
     style: ['normal', 'italic'], 
+    display: 'swap', 
   });
 export const nunito_sans = Nunito_Sans({
     subsets: ['latin'],
     weight: ['400', '700', '800'], 
     style: ['normal', 'italic'], 
+    display: 'swap', 
   });
+export const open_sans = Open_Sans({
+    subsets: ['latin'],
+    weight: ['400', '700', '800'], 
+    style: ['normal', 'italic'],
+    display: 'swap', 
+  });
+
+export const bricolageGrotesque = Bricolage_Grotesque({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+});
